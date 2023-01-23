d36705d7de686d92d5ca25f614bbba6970a6cf71
kilian <kilian.marmilliot@gmail.com>
2023-01-23 23:24:53 +0100

    test de cd

--- src/index.js
+++ src/index.js
@@ -10,9 +10,10 @@ import Offline from './components/Offline/Offline';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <BrowserRouter>
+          <App />
+          <Offline />
+      </BrowserRouter>
   </React.StrictMode>
 );
 
