47a900716db3332fa1251c10ab8dbb78e8b14f29
PikkMoune71 <antoinepicard385@gmail.com>
2023-01-24 12:51:41 +0100

    create conversation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 // import { ThemeProvider } from 'styled-components';
-import { Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home/Home';
-import  CreatePost  from './pages/CreatePost/CreatePost';
-import { Provider } from 'react-redux';
-import store from './store/store'
-import  Login  from './pages/Login/Login';
-import  Register  from './pages/Register/Register';
-import Chat from './pages/message/Message';
-import Conversation from './pages/conversation/Conversation';
-import { Navbar } from './components/molecules/Navbar/Navbar';
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Chat from "./pages/message/Message";
+import Conversation from "./pages/conversation/Conversation";
+import { Navbar } from "./components/molecules/Navbar/Navbar";
 import { ThemeProvider } from "@mui/material/styles";
-import createTheme from './themes/default.js'
+import createTheme from "./themes/default.js";
 import refreshTokenInterceptor from "./config/refreshTokenInterceptor";
 import UserProfile from "./pages/parametre/Paramtre";
+import CreateConversation from "./pages/CreateConversation/CreateConversation";
 
 function App() {
-
-    refreshTokenInterceptor(store);
+  refreshTokenInterceptor(store);
   return (
-      <Provider store={store} >
-        <ThemeProvider theme={createTheme}>
+    <Provider store={store}>
+      <ThemeProvider theme={createTheme}>
         <div className="App">
           <Navbar />
           <Routes>
@@ -30,13 +30,16 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/messages" element={<Chat />} />
+            <Route
+              path="/create-conversation"
+              element={<CreateConversation />}
+            />
             <Route path="/conversation/:id" element={<Conversation />} />
             <Route path="/parametre" element={<UserProfile />} />
           </Routes>
         </div>
-        </ThemeProvider>
-      </Provider>
-
+      </ThemeProvider>
+    </Provider>
   );
 }
 // primary secondary loading
--- src/slices/user.js
+++ src/slices/user.js
@@ -2,47 +2,67 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { setMessage } from "./printError";
 import usersService from "../services/user.service";
 
-
-export const getUserAuth = createAsyncThunk(
-    "users/me",
-    async (thunkAPI) => {
-        try {
-            const response = await usersService.getUserAuth();
-            //thunkAPI.dispatch(setMessage(response.data.message));
-            return response.data;
-        } catch (error) {
-            const message =
-                (error.response &&
-                    error.response.data &&
-                    error.response.data.message) ||
-                error.message ||
-                error.toString();
-            //thunkAPI.dispatch(setMessage(message));
-            return thunkAPI.rejectWithValue();
-        }
-    }
-);
-
-
-
-const usersSlice = createSlice({
-    name: "users",
-    initialState:{ me: null, listUsers:null},
-    extraReducers: {
-        [getUserAuth.rejected]: (state, action) => {
-            state.me = null;
-            return state;
-        },
-        [getUserAuth.fulfilled]: (state, action) => {
-            state.me = action.payload;
-            return state;
-        },
-        [getUserAuth.rejected]: (state, action) => {
-            state.me = null;
-            return state;
-        },
-    },
+export const getUserAuth = createAsyncThunk("users/me", async (thunkAPI) => {
+  try {
+    const response = await usersService.getUserAuth();
+    //thunkAPI.dispatch(setMessage(response.data.message));
+    return response.data;
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    //thunkAPI.dispatch(setMessage(message));
+    return thunkAPI.rejectWithValue();
+  }
 });
 
-const { reducer } = usersSlice ;
+export const getAllUsers = createAsyncThunk(
+  "users",
+  async (currentId, thunkAPI) => {
+    try {
+      const response = await usersService.getAllUsers(currentId);
+      //thunkAPI.dispatch(setMessage(response.data.message));
+      return response.data;
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      //thunkAPI.dispatch(setMessage(message));
+      return thunkAPI.rejectWithValue();
+    }
+  }
+);
+
+const usersSlice = createSlice({
+  name: "users",
+  initialState: { me: null, listUsers: null, users: null },
+  extraReducers: {
+    [getUserAuth.rejected]: (state, action) => {
+      state.me = null;
+      return state;
+    },
+    [getUserAuth.fulfilled]: (state, action) => {
+      state.me = action.payload;
+      return state;
+    },
+    [getUserAuth.rejected]: (state, action) => {
+      state.me = null;
+      return state;
+    },
+    [getAllUsers.fulfilled]: (state, action) => {
+      state.users = action.payload;
+      return state;
+    },
+    [getAllUsers.rejected]: (state, action) => {
+      state.users = null;
+      return state;
+    },
+  },
+});
+
+const { reducer } = usersSlice;
 export default reducer;
